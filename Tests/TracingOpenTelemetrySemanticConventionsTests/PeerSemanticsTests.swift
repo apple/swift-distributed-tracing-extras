60564dd5b5ff5f175b0f66eb032c04159a7ff1d7
Moritz Lang <hi@slashmo.codes>
2023-01-26 00:28:48 +0100

    Add OTel semantic peer span attributes

---
+++ Tests/TracingOpenTelemetrySemanticConventionsTests/PeerSemanticsTests.swift
@@ -0,0 +1,27 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift Distributed Tracing open source project
+//
+// Copyright (c) 2023 Apple Inc. and the Swift Distributed Tracing project
+// authors
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+import Tracing
+import TracingOpenTelemetrySemanticConventions
+import XCTest
+
+final class PeerSemanticsTests: XCTestCase {
+    func test_peer() {
+        var attributes = SpanAttributes()
+
+        attributes.peer.service = "orders"
+
+        XCTAssertSpanAttributesEqual(attributes, ["peer.service": "orders"])
+    }
+}
